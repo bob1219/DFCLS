a15733bd1bd25071a10e72b18f678c657930efce
吉田大輝 <bob181219@yahoo.co.jp>
2018-01-03 17:42:49 +0900

    Describe dfcls::CommandLine function

---
+++ CommandLine.cpp
@@ -0,0 +1,25 @@
+// Standard Libraries
+#include <string>
+#include <iostream>
+
+// Header
+#include "normal-function.h"
+
+using namespace std;
+using namespace dfcls;
+
+void CommandLine()
+{
+	string command;
+
+	while(true)
+	{
+		getline(cin, command);
+		if(CommandLine(command))
+			cout << "succeeded." << endl;
+		else
+			cerr << "failed." << endl;
+
+		cout << endl;
+	}
+}
