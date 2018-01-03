9b50a0f969c13912350bb133df44eac160333cbd
吉田大輝 <bob181219@yahoo.co.jp>
2018-01-03 12:55:25 +0900

    Addition a declaration of a constant "VERSION"

--- constant.h
+++ constant.h
@@ -1,14 +1,16 @@
 #ifndef _CONSTANT_H_
 #define _CONSTANT_H_
 
-// Standard Library
+// Standard Libraries
 #include <cstddef>
+#include <string>
 
 namespace dfcls
 {
 	static const char		PATH_BREAK_CHARACTER	= '\\';
 	static const std::size_t	DATE_MAX		= 22;
 	static const std::size_t	LOGTYPE_MAX		= 100;
+	static const std::string	VERSION			= "1.0";
 }
 
 #endif
