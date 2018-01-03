f7b3306f68e4c57ad687c59f3c33f3a835ed084b
吉田大輝 <bob181219@yahoo.co.jp>
2018-01-03 12:52:06 +0900

    Addition declarations of constants "PATH_BREAK_CHARACTER" and "DATE_MAX" and "LOGTYPE_MAX"

---
+++ constant.h
@@ -0,0 +1,14 @@
+#ifndef _CONSTANT_H_
+#define _CONSTANT_H_
+
+// Standard Library
+#include <cstddef>
+
+namespace dfcls
+{
+	static const char		PATH_BREAK_CHARACTER	= '\\';
+	static const std::size_t	DATE_MAX		= 22;
+	static const std::size_t	LOGTYPE_MAX		= 100;
+}
+
+#endif
