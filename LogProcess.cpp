152b2cf679e10b3270229723164dae0e7e541555
吉田大輝 <bob181219@yahoo.co.jp>
2018-01-03 12:41:13 +0900

    Describe dfcls::LogProcess::write function

--- LogProcess.cpp
+++ LogProcess.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <iostream>
 #include <fstream>
+#include <ctime>
 
 // Headers
 #include "class.h"
@@ -26,4 +27,23 @@ bool LogProcess::init()
 	if(fs.fail())return false;
 
 	return true;
+}
+
+void write(string type, string message)
+{
+	ofstream ofs(filename, ios_base::out | ios_base::app);
+	if(ofs.fail())
+	{
+		cout << "Error: Cannot open logfile \"" << filename << "\"." << endl;
+		return;
+	}
+
+	// write
+	time_t		timer = time(NULL);
+	struct tm	*local = localtime(&timer);
+	char date[DATE_MAX];
+	sprintf(date, "[%02d/%02d/%04d %02d:%02d:%02d]", local->tm_mon + 1, local->tm_mday, local->tm_year + 1900, local->tm_hour, local->tm_min, local->tm_sec);
+	char _type[LOGTYPE_MAX];
+	sprintf(_type, "[%s]", type);
+	ofs << date << _type << message << "." << endl;
 }
