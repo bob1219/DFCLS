3299f73ec6555bd0460346b4724ea57ed7ebd794
吉田大輝 <bob181219@yahoo.co.jp>
2018-01-03 12:58:24 +0900

    Modify LogProcess.cpp

--- LogProcess.cpp
+++ LogProcess.cpp
@@ -29,7 +29,7 @@ bool LogProcess::init()
 	return true;
 }
 
-void write(string type, string message)
+void LogProcess::write(string type, string message)
 {
 	ofstream ofs(filename, ios_base::out | ios_base::app);
 	if(ofs.fail())
