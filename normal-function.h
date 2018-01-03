e72f725d61a4d8e2bafabb9fd686e0559d816dba
吉田大輝 <bob181219@yahoo.co.jp>
2018-01-03 12:01:29 +0900

    Addition declarations of function "dfcls::prepering" and "dfcls::CommandLine" and "dfcls::welcome"

---
+++ normal-function.h
@@ -0,0 +1,28 @@
+#ifndef _NORMAL_FUNCTION_H_
+#define _NORMAL_FUNCTION_H_
+
+// Standard Library
+#include <iostream>
+
+// Headers
+#include "classes.h"
+#include "constant.h"
+#include "extern.h"
+
+namespace dfcls
+{
+	bool prepering();
+	void CommandLine();
+
+	inline void welcome()
+	{
+		std::cout << "Welcome to DFCLS version " << VERSION << std::endl;
+		if(WriteLog)
+		{
+			LogProcess log;
+			log.write("info", "DFCLS version " + VERSION);
+		}
+	}
+}
+
+#endif
