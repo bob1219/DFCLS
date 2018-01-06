ce74506ea9c0549d40da882a251d26bfc8eb59a9
吉田大輝 <bob181219@yahoo.co.jp>
2018-01-06 11:41:27 +0900

    Modify dfcls::LogProcess::init function

--- LogProcess.cpp
+++ LogProcess.cpp
@@ -18,8 +18,6 @@ string LogProcess::filename = "";
 bool LogProcess::init()
 {
 	char filename_c[FILENAME_MAX];
-	if(LogDirectory[strlen(LogDirectory) - 1] == PATH_BREAK_CHARACTER)
-		LogDirectory[strlen(LogDirectory) - 1] = '\0';
 	sprintf(filename_c, "%s%cDFCLS.log", LogDirectory, PATH_BREAK_CHARACTER);
 	filename = filename_c;
 
